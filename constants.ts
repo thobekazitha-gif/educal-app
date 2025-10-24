f7a74013d3a1b3f7d9e213fcdf8a66aaab6eee7b
thobekazitha-gif <thobeka.zitha@capaciti.org.za>
2025-10-24 12:25:35 +0200

    Create constants for MATERIAL_TYPES and GRADE_LEVELS
    Added constants for material types and grade levels.

---
+++ constants.ts
@@ -0,0 +1,25 @@
+import { MaterialType } from './types';
+
+export const MATERIAL_TYPES: MaterialType[] = [
+    // FIX: Change LessonPlan to UnitPlan to match the updated type.
+    MaterialType.UnitPlan,
+    MaterialType.StudyGuide,
+];
+
+export const GRADE_LEVELS: string[] = [
+    'Grade R',
+    'Grade 1',
+    'Grade 2',
+    'Grade 3',
+    'Grade 4',
+    'Grade 5',
+    'Grade 6',
+    'Grade 7',
+    'Grade 8',
+    'Grade 9',
+    'Grade 10',
+    'Grade 11',
+    'Grade 12',
+    'Undergraduate',
+    'Postgraduate',
+];
